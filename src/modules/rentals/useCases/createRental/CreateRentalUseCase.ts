1ad95ce1a1fc7ba115a78d511fe82d9b3497f196
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2022-06-17 09:56:24 -0300

    feature: add date provider

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,11 +1,7 @@
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc'
-
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
 import { AppError } from '@shared/errors/AppError';
-
-dayjs.extend(utc);
 
 interface IRequest {
   user_id: string;
@@ -16,6 +12,7 @@ interface IRequest {
 class CreateRentalUseCase {
   constructor(
     private rentalsRepository: IRentalsRepository,
+    private dateProvider: IDateProvider,
   ) {}
 
   async execute({ user_id, car_id, expected_return_date }: IRequest): Promise<Rental> {
@@ -24,7 +21,7 @@ class CreateRentalUseCase {
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(car_id);
 
     if (carUnavailable) {
-      throw new AppError('Car is unvailable');
+      throw new AppError('Car is unavailable');
     }
 
     const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(user_id);
@@ -33,11 +30,9 @@ class CreateRentalUseCase {
       throw new AppError("There's a rental in progress for user");
     }
 
-    const expectedReturnDateFormat = dayjs(expected_return_date).utc().local().format();
+    const dateNow = this.dateProvider.dateNow();
 
-    const dateNow = dayjs().utc().local().format();
-
-    const compare = dayjs(expectedReturnDateFormat).diff(dateNow, 'hours');
+    const compare = this.dateProvider.compareInHours(dateNow, expected_return_date);
 
     if (compare < minimumHour) {
       throw new AppError('Invalid return time');
